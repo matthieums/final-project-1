11a01765514d11877f3e1fff621fce159a67cb93
Matthieu <martens.matthieu@hotmail.com>
2024-04-25 14:27:33 +0200

    Add index page that queries for day offs and other data from the user

--- finalproject/holidaycalculator/views.py
+++ finalproject/holidaycalculator/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render("index.html")
+    return render(request, "holidaycalculator/index.html")
+
+def calendar(request):
+    return render(request, "holidaycalculator/calendar.html")
